0f968cbb6f6f520acb07387418315d08742b990c
yangzhongjie <853532673@qq.com>
2023-01-12 21:14:20 +0800

    界面布局

---
+++ src/components/sidebar/sidebar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const shadowGroup = ref([
+  {
+    name: 'Basic Shadow',
+    type: '',
+  },
+]);
+// const getCssVarName = (type: string) => {
+//   return `--el-box-shadow${type ? '-' : ''}${type}`;
+// };
+
+</script>
+
+<template>
+  <div class="sidebar">
+    <h1>11</h1>
+    <ul>
+      <li>用户管理</li>
+      <li>账户管理</li>
+      <li>密码管理</li>
+      <li>个性签名</li>
+    </ul>
+    <ul>
+      <li>权限管理</li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul{
+  padding: 0;
+}
+</style>
