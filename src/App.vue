861ef16d0027add5ad51de3a5f134d41df8f9b55
yangzhongjie <aokieoog@gmail.com>
2023-01-13 17:54:57 +0800

    布局重建

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,68 @@
 <script setup>
-import Header from './components/sidebar/sidebar.vue'
+import Sidebar from './components/sidebar/sidebar.vue'
 </script>
 
 <template>
 
     <div class="common-layout">
       <el-container>
-        <el-aside width="200px">
-          <Header></Header>
-        </el-aside>
-        <el-container>
-          <el-header>Header</el-header>
-          <el-main width="200px" height="500px">
-              <!-- 传一个组件在router-view中 -->
-              <router-view  v-slot="{ Component }">
-                <keep-alive>
-                  <component :is="Component"></component>
-                </keep-alive>
-              </router-view>
+        <!--头部-->
+        <el-header>Header</el-header>
+        <div>
+          <!--左菜单侧栏-->
+          <el-aside>
+            <Sidebar></Sidebar>
+          </el-aside>
+          <!--路由内容区-->
+          <el-main>
+            <!-- 传一个组件在router-view中 -->
+            <router-view v-slot="{ Component }">
+              <keep-alive>
+                <component :is="Component"></component>
+              </keep-alive>
+            </router-view>
           </el-main>
-        </el-container>
+        </div>
       </el-container>
     </div>
 
 </template>
 
-<style scoped>
+<style>
+html, body {
+  font-size:16px
+}
+body {
+  background-color:#efefef;
+}
+#app{
+  height: 100vh;
+}
 .common-layout{
   padding: 0 !important;
 }
 .el-container{
-  width: 1450px;
+  width: 73.125rem;
 }
 .el-aside{
   height: 100vh;
-  width: 260px;
+  width: 13.75rem;
   position: fixed;
   background: #f9f9f9;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px;
 }
 .el-header{
   position: fixed;
-  height: 50px;
-  width: 1450px;
+  height: 3.125rem;
+  width: 73.125rem;
   z-index: 10;
   background: #f9f9f9;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 }
 .el-main{
-  height: 100%;
+  /* calc 可以用来计算加减乘除 */
+  width: calc(73.125rem - 13.75rem);
+  float: right;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,22 @@
 <script setup>
 import { ref } from 'vue';
 
-defineProps({
-  msg: String,
-});
+// 定义数据类型
+// defineProps({
+//   msg: String,
+// });
 const count = ref(0);
+const msg = ref('内容')
 </script>
 
 <template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
+  <h1>{{ msg }}</h1>
   <h1>{{ msg }}</h1>
 
   <div class="card">
