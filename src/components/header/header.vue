c1a4772ed1110ecd880eb8196a6891fc893d7518
yangzhongjie <aokieoog@gmail.com>
2023-02-14 17:49:33 +0800

    BUG-搜索组件样式穿透到头部组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,19 @@
-<script setup></script>
+<script setup>
+import SearchBox from '../searchBox/searchBox.vue'</script>
 
 <template>
-  <img alt="" src="https://www.ihewro.com/logo.svg" type="image/svg+xml">
+  <div>
+    <img alt="" src="https://www.ihewro.com/logo.svg" type="image/svg+xml">
+  </div>
+  <div>
+    <SearchBox></SearchBox>
+  </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 img {
+  float: left;
   width: 130px;
   height: 40px;
-  float: left;
 }
 </style>
--- src/components/searchBox/searchBox.vue
+++ src/components/searchBox/searchBox.vue
@@ -3,10 +3,10 @@ import SvgIcon from '../svgIconFont/svgIcon.vue'</script>
 
 <template>
   <div>
-    <el-input></el-input>
-    <span>
+    <input placeholder="搜索" />
+    <el-button plain type="">
       <svg-icon icon-name="icon-sousuo"></svg-icon>
-    </span>
+    </el-button>
   </div>
 
 </template>
@@ -15,16 +15,33 @@ import SvgIcon from '../svgIconFont/svgIcon.vue'</script>
 div {
   width: 12.375rem;
   height: 1.875rem;
-  border: 1px solid black;
   border-radius: 0.9375rem;
-  .el-input {
-    width: 10.125rem;
-    height: 1.875rem;
-    border: none;
-  }
-  span {
-    width: 2.1875rem;
-    height: 1.875rem;
-  }
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  background-color: #ffffff;
+}
+input {
+  float: left;
+  width: 9.25rem;
+  height: 1.875rem;
+  border-radius: 0.9375rem 0 0 0.9375rem;
+  border: none;
+  padding: 0 0 0 0.9375rem;
+}
+input:hover {
+  background-color: #e0e6ed;
+}
+input:focus {
+  outline: 1px solid #448bff; //边框不用border，用outline
+}
+.el-button {
+  float: right;
+  width: 2.1875rem;
+  height: 1.875rem;
+  font-weight: 700;
+  border: none;
+  border-radius: 0 0.9375rem 0.9375rem 0;
+}
+.el-button:active{
+  box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset;
 }
 </style>
