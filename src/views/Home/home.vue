ca6ae5c861016850a5b1ec1c1ab720787b62687e
yangzhongjie <853532673@qq.com>
2023-03-28 00:20:49 +0800

    首页图片

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -4,14 +4,31 @@
  * @作者: Aoki
  * @时间: 2023/02/17 17:22:22
  */
-import mock from '@/mock/mockjs'
+import mock from '@/mock/mockjs';
+import { getCurrentInstance, onMounted, ref } from 'vue';
 
-console.log('dd+', mock.mockid())
+const { proxy } = getCurrentInstance();
+const title = ref('博客君');
+let listImage = ref([])
+console.log('dd+', mock.mockid());
+onMounted(() => {
+  proxy.$http.get('/dev/api/image/girl/list/random?count=1&app_id=l8lesimwu6oumkj9&app_secret=cWdQQnp0Nnd6QUpweFJhM2F1L0ZwZz09').then((res) => {
+    if (res.data.code === 1) {
+      listImage.value=res.data.data
+    }
+  });
+});
+
 </script>
 
 
 <template>
-  <h1>博客君</h1>
+  <h1>{{ title }}</h1>
+  <div class="image">
+    <div class="image_box" v-for="item in listImage.slice(0,9)">
+      <img style="width: 300px;height: 150px" :src="item.imageUrl" alt="">
+    </div>
+  </div>
 </template>
 
 
@@ -19,5 +36,13 @@ console.log('dd+', mock.mockid())
 .about {
   height: 400px;
   width: 100%;
+}
+.image{
+  display: inline-flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.image_box img{
+  display: inline-block;
 }
 </style>
