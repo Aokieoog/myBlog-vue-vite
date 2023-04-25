0719131e55a950d848e694c3671de3c8fcb4e7b7
yangzhongjie <aokieoog@gmail.com>
2023-04-25 10:30:25 +0800

    7天过期

--- src/utils/http/cookie.js
+++ src/utils/http/cookie.js
@@ -1,8 +1,9 @@
 import generateToken from '@/utils/http/token.js'
+
 const token = generateToken()
 const Cookie = function () {
   // 登录成功后，服务器返回 Token，并将其存储在 Cookie 中
-  const expires = new Date(Date.now() + 50000) // 设置有效期
+  const expires = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 设置有效期
   document.cookie = `token=${token}; expires=${expires.toUTCString()}; path=/`
 }
 export default Cookie
