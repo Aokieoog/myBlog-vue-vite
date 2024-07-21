e0683e2c541d56a3f077b67ced062cc7ba0c8997
yangzhongjie <853532673@qq.com>
2024-07-21 23:06:47 +0800

    bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import util from '@/utils/util'
 const routes = [
   {
@@ -117,7 +117,7 @@ const routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes
 })
 
