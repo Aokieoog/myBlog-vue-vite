d864dedba8b91f743c24751d66f071af1dc7e536
yangzhongjie <853532673@qq.com>
2024-07-21 23:08:12 +0800

    还原

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import util from '@/utils/util'
 const routes = [
   {
@@ -117,7 +117,7 @@ const routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: routes
 })
 
