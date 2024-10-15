d8d938a63ff0c61eacc26fd3bfbfe58cc2917d57
yangzhongjie <853532673@qq.com>
2024-10-15 22:03:34 +0800

    赞赏

--- src/views/Home/jx3home.vue
+++ src/views/Home/jx3home.vue
@@ -1,28 +1,35 @@
 <template>
-  <div>
+  <div class="container">
     <h1>感谢使用剑侠3记账本</h1>
-    
     <h2>公告</h2>
     <el-card style="width: 40rem;height: 30rem;" @click="gobook">
+      <el-divider content-position="left">2024年10月15日</el-divider>
+      <p>增加了外观统计页面</p>
+      <p>使用了废牛记账本的API，因此与<span style="color: red;">废牛记账本数据互通</span>。</p>
+      <p><span style="color: red;">PS：</span>如果在双梦遇到一个叫‘<span style="color: red;">咩气气</span>’的纯阳请给她一串糖葫芦。😊</p>
       <el-divider content-position="left">2024年7月23日</el-divider>
       <p>修改收益计算规则,修改后收益扣除交易行<span style="color: red;">5%</span>的手续费</p>
-
-      <el-divider content-position="left">2024年7月23日</el-divider>
       <p>“剑侠3记账本”是为剑网三做的一个交易行记账功能</p>
       <p>在某一天，逛交易行的时候发现忘记远古时期购买的材料单价，因此有感而发开发了此网站。</p>
       <p>此网站完全使用静态部署，所有的数据信息全部储存在本地浏览器，<span style="color: red;">不会窃取信息</span>。</p>
       <p>同样有弊端，不可以跨设备使用,浏览器缓存清除数据消失（<span style="color: red;">不手动清除不会消失</span>）。</p>
     </el-card>
-    <p>使用方法:<el-link class="url" href="http://qs.ieoog.com/index.php/archives/28/"
-      target="_blank">点我</el-link></p>
+    <p>使用方法:<el-link class="url" href="http://qs.ieoog.com/index.php/archives/28/" target="_blank">点我</el-link></p>
   </div>
+  <div class="img">
+      <el-image style="width: 6.25rem; height: 6.25rem" :src="image"></el-image>
+      <p>糖葫芦🍬😄</p>
+      <!-- <el-image style="width: 100px; height: 100px" :src="image"></el-image> -->
+  </div>
+
+
 </template>
 
 <script setup>
 import router from '@/router';
 import { ref, reactive } from 'vue';
 
-const data = reactive({})
+const image = ref('/src/assets/png/miemie.png')
 
 const gobook = () => {
   router.push({ path: '/js3book' })
@@ -36,4 +43,22 @@ const gobook = () => {
   color: #409EFF;
   font-size: 18px;
 }
+.container{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.img {
+  width: 9.375rem;
+  height: 10.75rem;
+  position: fixed;
+  bottom:1.25rem;
+  right: 1.25rem;
+  }
 </style>
